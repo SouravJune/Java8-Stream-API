b918d54e06c4e437b0f3b7457e8689cb36ede791
SouravJune <106313049+SouravJune@users.noreply.github.com>
2024-04-30 22:44:42 +0530

    SumOfAllDigits.java

---
+++ Stream-API-Interview-Coding-Examples/src/SumOfAllDigits.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/** Question: Java 8 Program to Find the Sum of All Digits of a Number */
+public class SumOfAllDigits {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many numbers do you want to add? ");
+        int numCount = sc.nextInt();
+        List<Integer> numberList = new ArrayList<>();
+
+        System.out.println("Enter numbers: ");
+        for(int i = 0; i < numCount; i++){
+            numberList.add(sc.nextInt());
+        }
+
+        Integer totalSum = numberList.stream()
+                .reduce(0, Integer::sum);
+
+        System.out.println("Total sum of numbers is: " + totalSum);
+    }
+}
