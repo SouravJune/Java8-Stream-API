5e7494be96c743e483c6a4d0fabcfffe5b242ed4
SouravJune <106313049+SouravJune@users.noreply.github.com>
2024-05-01 01:00:46 +0530

    PrintEvenNumbers.java

---
+++ Stream-API-Interview-Coding-Examples/src/PrintEvenNumbers.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class PrintEvenNumbers {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many numbers do you want to add? ");
+        int numCount = sc.nextInt();
+        List<Integer> numberList = new ArrayList<>();
+
+        System.out.println("Enter numbers: ");
+        for(int i = 0; i < numCount; i++){
+            numberList.add(sc.nextInt());
+        }
+
+        List<Integer> evenNumberList = numberList.stream()
+                .filter(number -> number % 2 == 0)
+                .toList();
+
+        System.out.println("Even numbers are: " + evenNumberList);
+    }
+}
