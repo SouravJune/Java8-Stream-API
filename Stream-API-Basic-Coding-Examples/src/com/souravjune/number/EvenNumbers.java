6d3377312a2ae43332135bac5d04906e5b916d9a
SouravJune <106313049+SouravJune@users.noreply.github.com>
2024-04-30 20:17:14 +0530

    EvenNumbers.java

---
+++ Stream-API-Basic-Coding-Examples/src/com/souravjune/number/EvenNumbers.java
@@ -0,0 +1,12 @@
+package com.souravjune.number;
+
+import java.util.Scanner;
+
+public class EvenNumbers {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
