9a87bac00de93d88ad6d791a97ef059cf3695837
SouravJune <106313049+SouravJune@users.noreply.github.com>
2024-05-01 09:05:25 +0530

    RetrieveLastElement.java

---
+++ Stream-API-Interview-Coding-Examples/src/RetrieveLastElement.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/** Question: Java 8 Program to Retrieve Last Element of a List of Strings */
+public class RetrieveLastElement {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many strings do you want to enter? ");
+        int numCount = sc.nextInt();
+        sc.nextLine();
+        List<String> numberList = new ArrayList<>();
+
+        System.out.println("Enter strings: ");
+        for(int i = 0; i < numCount; i++){
+            numberList.add(sc.nextLine());
+        }
+
+        numberList.stream()
+                .reduce((first, second) -> second)
+                .ifPresent(number -> System.out.println("last number is: " + number));
+    }
+}
