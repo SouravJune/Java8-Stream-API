536565bc46467f0996f07d2c813feba83199a8d7
SouravJune <106313049+SouravJune@users.noreply.github.com>
2024-09-02 10:43:32 +0530

    Create Transaction.java

---
+++ Stream-API-Methods-Usage/src/com/souravjune/model/Transaction.java
@@ -0,0 +1,59 @@
+package com.souravjune.model;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Transaction {
+    private int id;
+    private String description;
+    private double amount;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Transaction(int id, String description, double amount) {
+        this.id = id;
+        this.description = description;
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
+
+    public static List<Transaction> getTransactions() {
+        return Arrays.asList(
+                new Transaction(1, "Payment to Supplier", 1500.0),
+                new Transaction(2, "Refund", 1200.0),
+                new Transaction(3, "Purchase", 1100.0),
+                new Transaction(4, "Consulting Fee", 2000.0)
+        );
+    }
+}
+
