2a6b5a2fdb8d9c9c78eeba87ec91066ad8c9a50c
SouravJune <106313049+SouravJune@users.noreply.github.com>
2024-09-14 08:58:14 +0530

    Create FindMinimumOccurringCharacter .java

---
+++ Stream-API-Interview-Coding-Examples/src/com/souravjune/programs/FindMinimumOccurringCharacter.java
@@ -0,0 +1,32 @@
+package com.souravjune.programs;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/** Question: Write a Java 8 program to find the minimum occurring character in a given string */
+public class FindMinimumOccurringCharacter {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter string to check min character frequency: ");
+        String str = sc.nextLine();
+
+        Optional<Map.Entry<Character, Long>> duplicateCharactersFrequency = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase()
+                .chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))
+                .entrySet().stream()
+                .min(Map.Entry.comparingByValue()); // .min(Comparator.comparing(Map.Entry::getValue));
+
+        duplicateCharactersFrequency.ifPresent((entry) ->
+                System.out.println("Character: " + entry.getKey() + ", Count: " + entry.getValue()));
+
+
+    }
+}
+
